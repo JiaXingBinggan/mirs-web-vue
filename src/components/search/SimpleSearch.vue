49eed72f560902040de17160699215f741c836d3
邓 <1351650853@qq.com>
2016-11-29 11:49:52 +0800

    完成简单搜索对后台的数据请求

--- src/components/search/SimpleSearch.vue
+++ src/components/search/SimpleSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="simple-search" @keyup.enter="onSearch">
     <mu-text-field v-model="searchKeywords" class="appbar-search-field"  slot="right" hintText="请输入搜索内容"/>
-    <div class="search-suggest">
+    <div v-if="suggestions.length" class="search-suggest">
       <mu-list>
         <mu-list-item :title="suggestion.name" @click="goToMovie(suggestion.url)" v-for="suggestion in suggestions">
           <img :src="suggestion.pic" slot="left">
@@ -12,33 +12,14 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import movieApi from '../../api/movieApi'
 export default {
   name: 'simple-search',
   data () {
     return {
       searchKeywords: '',
-      suggestions: [
-        {
-          'url': '268',
-          'name': '黑板 第1夜 ブラックボード 第1夜',
-          'pic': 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2339240482.jpg'
-        },
-        {
-          'url': '813',
-          'name': 'BBC 巴黎 第一季 BBC Paris Season 1',
-          'pic': 'https://img1.doubanio.com/lpic/s8989209.jpg'
-        },
-        {
-          'url': '719',
-          'name': '热浪来袭！猫之谷！！ 熱いぞ!!猫ヶ谷',
-          'pic': 'https://img3.doubanio.com/lpic/s8965754.jpg'
-        },
-        {
-          'url': '493',
-          'name': '山下智久ROUTE66~只身一人的美国 山下智久・ルート66〜たった一人のアメリカ',
-          'pic': 'https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2284691247.jpg'
-        }
-      ]
+      suggestions: []
     }
   },
   methods: {
@@ -47,6 +28,32 @@ export default {
     },
     onSearch () {
       this.$router.push('/search?keywords=' + this.searchKeywords)
+    },
+    searchMovie: _.debounce(
+      function () {
+        let _this = this
+        movieApi.searchMovie(this.searchKeywords)
+        .then(function (res) {
+          if (res.data) {
+            _this.suggestions = res.data
+          }
+        })
+        .catch(function (res) {
+          if (res instanceof Error) {
+            window.console.log(res.message)
+          } else {
+            window.console.log(res.data)
+          }
+        })
+      },
+      // 等待一秒，限制请求频率
+      1000
+    )
+  },
+  watch: {
+    searchKeywords () {
+      // 当用户输入发生变化时，向后台请求数据
+      this.searchMovie()
     }
   }
 }
