0c5c8c586f8a84dbab8c41f7b426336fa14d6027
unknown <>
2016-12-09 10:29:56 +0800

    继续完善

--- src/views/AccountsView.vue
+++ src/views/AccountsView.vue
@@ -1,32 +1,44 @@
 <template>
   <div class="accounts-view">
-    <div id="form">
-      <h2>你的信息</h2>
-      <div class="face-area">
-        <img class="face" height="200" width="200" src="https://avatars0.githubusercontent.com/u/17798805?v=3&u=a85ecf41aa0ef94009bfd4dcea7f3208069dcf16&s=400">
-        <input type="button" class="upload-face input-button" @click="uploadFace" value="爆照">
-      </div>
-      <span>用户名</span><br><input class="input-line" v-modle="userName" v-bind:value="userName">
-      <br><br>
-      <input type="radio" id="male" value="male" v-model="sex">
-      <label for="male">帅哥</label>
-      <input type="radio" id="female" value="female" v-model="sex">
-      <label for="female">美女</label>
-      <br><br>
-      <span>邮箱</span><br><input class="input-line" v-modle="email" v-bind:value="email">
-      <br><br>
-      <span>所在地</span><br><input class="input-line" v-modle="address" v-bind:value="address">
-      <br><br>
-      <span>大学</span><br><input class="input-line" v-modle="college" v-bind:value="college">
-      <br><br>
-      <span>专业</span><br><input class="input-line" v-modle="major" v-bind:value="major">
-      <br><br>
-      <span>座右铭</span><br><textarea class="input-text" v-modle="motto" v-bind:value="motto"></textarea>
-      <br><br>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="input-button" @click="submit()" value="保存">
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <input type="button" class="input-button" @click="exit()" value="退出">
-    </div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="menu">
+          <h2>信息设置</h2>
+          <a class="menu-item" href="#">你的信息</a>
+          <a class="menu-item" href="#">更改密码</a>
+        </div>
+      </el-col>
+      <el-col :span="18">
+        <div id="form">
+          <div class="face-area">
+            <h3>你的帅脸</h3>
+            <img class="face" height="200" width="200" src="https://avatars0.githubusercontent.com/u/17798805?v=3&u=a85ecf41aa0ef94009bfd4dcea7f3208069dcf16&s=400">
+            <input type="button" class="upload-face input-button" @click="uploadFace" value="爆照">
+          </div>
+          <h3>{{title}}</h3>
+          <span>用户名</span><br><input class="input-line" v-modle="userName" v-bind:value="userName">
+          <br><br>
+          <input type="radio" id="male" value="male" v-model="sex">
+          <label for="male">帅哥</label>
+          <input type="radio" id="female" value="female" v-model="sex">
+          <label for="female">美女</label>
+          <br><br>
+          <span>邮箱</span><br><input class="input-line" v-modle="email" v-bind:value="email">
+          <br><br>
+          <span>所在地</span><br><input class="input-line" v-modle="address" v-bind:value="address">
+          <br><br>
+          <span>大学</span><br><input class="input-line" v-modle="college" v-bind:value="college">
+          <br><br>
+          <span>专业</span><br><input class="input-line" v-modle="major" v-bind:value="major">
+          <br><br>
+          <span>座右铭</span><br><textarea class="input-text" v-modle="motto" v-bind:value="motto"></textarea>
+          <br><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="input-button" @click="submit()" value="保存">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="button" class="input-button" @click="exit()" value="退出">
+        </div>
+      </el-col>
+    </el-row>
 </template>
 
 <script>
@@ -40,6 +52,7 @@ export default {
   },
   data () {
     return {
+      title: '你的信息',
       userName: 'Jack',
       sex: 'male',
       email: '1140746804@qq.com',
@@ -61,34 +74,55 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-h2
-  margin-bottom 10px
-.input-button
-  border-radius 3px
-  width 80px
-  background-color #eee
-  border 1px solid #ddd
-#form
+.accounts-view
+  width 980px
+  margin-top 50px
   margin-left auto
   margin-right auto
-  width 980px
-  padding 0px 50px 50px 180px
-  .input-line, .input-text
-    width 440px
-    margin-top 6px
-    padding 6px 8px
-    border 1px solid #ddd
+  padding 0
+  padding-bottom 60px
+  .menu
+    border 1px solid #d8d8d8
     border-radius 3px
-    background-color #fafafa
-    box-shadow inset 0 1px 2px rgba(0, 0, 0, 0.075)
-  .input-text
-    height 90px
-  .face-area
-    float right
-    img
+    h2
+      margin-top 0
+      margin-bottom 0
+      padding 8px 10px 8px 10px
+      border-bottom 1px solid #eee
+      border-top-radius 2px
+      background-color #f7f7f7
+    .menu-item
       display block
-      margin-bottom 20px
-      border-radius 6px
-    .upload-face
-      width 100%
+      padding 8px 10px 8px 10px
+      border-bottom 1px solid #eee
+    a
+      color #111
+  .input-button
+    border-radius 3px
+    width 80px
+    background-color #eee
+    border 1px solid #ddd
+  #form
+    margin-left auto
+    margin-right auto
+    padding-left 20px
+    .input-line, .input-text
+      width 360px
+      margin-top 6px
+      padding 6px 8px
+      border 1px solid #ddd
+      border-radius 3px
+      background-color #fafafa
+      box-shadow inset 0 1px 2px rgba(0, 0, 0, 0.075)
+    .input-text
+      height 90px
+    .face-area
+      float right
+      margin-right 40px
+      img
+        display block
+        margin-bottom 20px
+        border-radius 6px
+      .upload-face
+        width 100%
 </style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="user-view">
-    <h1>{{paramId}}</h1>
+    <!-- <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="menu">
+          <h2>用户信息</h2>
+          <a class="menu-item" href="#">基础信息</a>
+          <a class="menu-item" href="#">他的电影</a>
+        </div>
+      </el-col>
+      <el-col :span="18"> -->
+        <div id="form">
+          <div class="face-area">
+            <h3>他的帅脸</h3>
+            <img class="face" height="200" width="200" src="https://avatars0.githubusercontent.com/u/17798805?v=3&u=a85ecf41aa0ef94009bfd4dcea7f3208069dcf16&s=400">
+            <input type="button" class="upload-face input-button" @click="uploadFace" value="爆照">
+          </div>
+          <h3>{{title}}</h3>
+          <span class="info-title">用户名：</span><span class="info-detail">{{userName}}</span><br>
+          <span class="info-title">邮箱：</span><span class="info-detail">{{email}}</span><br>
+          <span class="info-title">所在地：</span><span class="info-detail">{{address}}</span><br>
+          <span class="info-title">学校：</span><span class="info-detail">{{college}}</span><br>
+          <span class="info-title">专业：</span><span class="info-detail">{{major}}</span><br>
+          <span class="info-title">座右铭：</span><span class="info-detail">{{motto}}</span><br>
+      <!-- </el-col>
+    </el-row> -->
     <template v-if="user">
       <h1>User : {{ user.id }}</h1>
       <ul class="meta">
@@ -17,7 +40,14 @@ export default {
   name: 'user-view',
   data () {
     return {
-      paramId: 'user id from params:' + this.$route.params.id
+      paramId: 'user id from params:' + this.$route.params.id,
+      userName: 'Jack',
+      email: '1140746804@qq.com',
+      address: '成都市',
+      college: '电子科技大学',
+      major: '软件技术',
+      motto: '“你的名字。”非常好看！',
+      title: '基础信息'
     }
   },
   computed: {
@@ -34,20 +64,36 @@ export default {
 
 <style lang="stylus" scoped>
 .user-view
+  width 720px
+  margin-top 50px
+  margin-left auto
+  margin-right auto
+  padding 0
+  padding-bottom 60px
+  .menu
+    border 1px solid #d8d8d8
+    border-radius 3px
+    h2
+      margin-top 0
+      margin-bottom 0
+      padding 8px 10px 8px 10px
+      border-bottom 1px solid #eee
+      border-top-radius 2px
+      background-color #f7f7f7
+    .menu-item
+      display block
+      padding 8px 10px 8px 10px
+      border-bottom 1px solid #eee
   background-color #fff
   box-sizing border-box
   padding 2em 3em
-  h1
-    margin 0
-    font-size 1.5em
-  .meta
-    list-style-type none
-    padding 0
-  .label
-    display inline-block
-    min-width 4em
-  .about
-    margin 1em 0
-  .links a
-    text-decoration underline
+  .face-area
+    float right
+    margin-right 40px
+    img
+      display block
+      margin-bottom 20px
+      border-radius 6px
+    .upload-face
+      width 100%
 </style>
