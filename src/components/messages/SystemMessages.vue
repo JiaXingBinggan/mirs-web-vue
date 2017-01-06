6d18f9ccd8d12d1bbdfa09d0bcfc50502426520f
JiaXingBingGan <729869585@qq.com>
2017-01-06 18:54:34 +0800

    questionaire modify

--- src/components/messages/SystemMessages.vue
+++ src/components/messages/SystemMessages.vue
@@ -65,7 +65,7 @@ export default {
 </script>
 <style lang="stylus" scoped>
 .message-container
-	width 350px 
+	width 360px 
 	border 1px solid #E0E0E0
 	-webkit-border-radius 8px
 	-moz-border-radius 8px
